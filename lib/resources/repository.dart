a948d0c45127fe9f5f4e053cd7e022242c1f70d8
manek11 <54968278+manek11@users.noreply.github.com>
2021-02-22 10:50:10 +0530

    merged changes

--- lib/resources/repository.dart
+++ lib/resources/repository.dart
@@ -4,7 +4,7 @@ Future<void> signIn(String email, String password) async {}
 
 Future<void> register(String email, String password) async {
   try {
-    await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password)
+    await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
   } on FirebaseAuthException catch (e) {
     if (e.code == 'weak-password') {
       print('The password provided is too weak');
