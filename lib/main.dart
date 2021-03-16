6a36b4106543dd4f6492f562acccd07470433bca
So Nozaki <sou.nozaki@gmail.com>
2021-03-16 15:53:28 +0900

    main.dart modified

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:parking_app/screens/profile_page/profile_page.dart';
 import 'package:parking_app/screens/qrscanner_page/qrscanner_page.dart';
 import 'package:parking_app/screens/timer_page/timer_page.dart';
 
+/*
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -24,6 +25,90 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'ParKing',
       home: RootPage(),
+    );
+  }
+}
+*/
+
+
+
+
+/*
+ *
+ *
+ *
+ */
+
+
+void main() async {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: MyApp(),
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+
+    animation = CurvedAnimation(
+      parent: controller,
+      curve: Curves.easeInOutCubic,
+    ).drive(Tween(begin: 0, end: 2));
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        controller
+          ..reset()
+          ..forward();
+      },
+      child: RotationTransition(
+        turns: animation,
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: FlutterLogo(),
+            ),
+            Center(
+              child: Text(
+                'Click me!',
+                style: TextStyle(
+                  fontSize: 60.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
