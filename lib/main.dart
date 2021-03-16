7d5478c568990a1e558373f2ff83bf06e0300bba
sanjeev <sanjeevk1999@gmail.com>
2021-03-16 12:35:18 +0530

    fixed main

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ import 'package:provider/provider.dart';
 
 import 'DataHandler/appData.dart';
 
-/*
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -30,90 +29,6 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'ParKing',
         home: RootPage(),
-      ),
-    );
-  }
-}
-*/
-
-
-
-
-/*
- *
- *
- *
- */
-
-
-void main() async {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: MyApp(),
-      ),
-    ),
-  );
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp>
-    with SingleTickerProviderStateMixin {
-  late AnimationController controller;
-  late Animation<double> animation;
-
-  @override
-  void initState() {
-    super.initState();
-
-    controller = AnimationController(
-      duration: Duration(seconds: 1),
-      vsync: this,
-    );
-
-    animation = CurvedAnimation(
-      parent: controller,
-      curve: Curves.easeInOutCubic,
-    ).drive(Tween(begin: 0, end: 2));
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        controller
-          ..reset()
-          ..forward();
-      },
-      child: RotationTransition(
-        turns: animation,
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: FlutterLogo(),
-            ),
-            Center(
-              child: Text(
-                'Click me!',
-                style: TextStyle(
-                  fontSize: 60.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
